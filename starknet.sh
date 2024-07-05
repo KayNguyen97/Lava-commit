a76f8d1f41afb3209ce39d5779c6b20a72e66df9
Kay Nguyen/WVN/Wistron <Kay_Nguyen@wistron.com>
2024-07-05 11:15:59 +0700

    1

---
+++ starknet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://g.w.lavanet.xyz:443/gateway/strk/rpc-http/0e2c68fc9f82cfcb51efdeafa6fe6e71"
+
+OUTPUT_DIR="logs"
+
+starknet_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"starknet_blockNumber"}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$starknet_data" >> "$OUTPUT_DIR/starknet.txt"
+    
+    echo "StarkNet data fetched and saved to $OUTPUT_DIR/starknet.txt."
+else
+    echo "Failed to fetch StarkNet data."
+fi
